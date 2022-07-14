3787d8ae770e126c24048b6685f2d3bede12c9dd
Bogdan Simon <Bogdan.Simon@rebeldot.com>
2022-07-14 21:45:00 +0300

    docs(sdk): update map.js

--- docs/pages/api/map.js
+++ docs/pages/api/map.js
@@ -7,14 +7,14 @@ layout: page
 language:
 - JavaScript
 products:
-- MapLibre GL JS
+- UNL Map JS
 ---*/
 
-import React from 'react';
-import PageWrapper from '../../components/api/page-wrapper.js';
+import React from "react";
+import PageWrapper from "../../components/api/page-wrapper.js";
 
 export default class Page extends React.Component {
-    render() {
-        return <PageWrapper name="Map class" {...this.props} />;
-    }
+  render() {
+    return <PageWrapper name="Map class" {...this.props} />;
+  }
 }
