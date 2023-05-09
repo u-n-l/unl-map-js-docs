35c3071e555a754fcce51ff3b70ef54cb4405ea4
Bogdan Simon <Bogdan.Simon@rebeldot.com>
2023-05-09 13:54:31 +0300

    Increment version

--- docs/components/urls.js
+++ docs/components/urls.js
@@ -1,4 +1,4 @@
-export const UNL_MAP_JS_VERSION = "0.1.1";
+export const UNL_MAP_JS_VERSION = "0.1.2";
 
 function url(ext) {
   return `https://unpkg.com/unl-map-js@${UNL_MAP_JS_VERSION}/lib/unl-map-js.${ext}`;
